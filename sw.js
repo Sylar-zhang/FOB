64e6fee0c1e253c0a9ce61a671e45f296ebb87f7
Sylar-zhang <zhang.jch123@gmail.com>
2026-01-14 15:57:15 +0800

    Update service worker cache and asset handling

--- sw.js
+++ sw.js
@@ -1,19 +1,20 @@
-const CACHE_NAME="fob-pwa-v1";
-const FILES=[
-  "./",
-  "./index.html",
-  "./manifest.json",
-  "./sw.js",
-  "./icon-192.png",
-  "./icon-512.png"
+const CACHE_NAME = 'fob-calc-v1';
+const ASSETS = [
+  './',
+  './index.html',
+  './manifest.json',
+  './icon-192.png',
+  './icon-512.png'
 ];
 
-self.addEventListener("install",e=>{
-  e.waitUntil(caches.open(CACHE_NAME).then(c=>c.addAll(FILES)));
+self.addEventListener('install', (e) => {
+  e.waitUntil(
+    caches.open(CACHE_NAME).then((cache) => cache.addAll(ASSETS))
+  );
 });
 
-self.addEventListener("fetch",e=>{
+self.addEventListener('fetch', (e) => {
   e.respondWith(
-    caches.match(e.request).then(r=>r||fetch(e.request))
+    caches.match(e.request).then((response) => response || fetch(e.request))
   );
 });
